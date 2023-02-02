32397cdb70bfa664c4a5be6a9096c39c0f5fcfd6
oagomezz <ogomezbarragan437@gmail.com>
2023-02-02 15:26:34 -0800

    completed create functionality

--- src/Pages/DogNew.js
+++ src/Pages/DogNew.js
@@ -1,10 +1,57 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Form, FormGroup, Label, Input, Button } from "reactstrap"
+import { useNavigate } from "react-router-dom"
 
-const DogNew = () => {
+
+const DogNew = ({ createDog }) => {
+  const navigate = useNavigate()
+  const [newDog, setNewDog] = useState({
+    name: "",
+    age: "",
+    enjoys: "",
+    image: ""
+  })
+  const handleChange = (e) => {
+    setNewDog({ ...newDog, [e.target.name]: e.target.value })
+  }
+  const handleSubmit = () => {
+    createDog(newDog)
+    navigate("/dogindex")
+  }
 
   return(
-    <> <h1>This is the DogNew</h1></>
-  )
+    <> <h1>Add a New Dog!</h1>
+      <Form>
+        <FormGroup>
+          <Label for="name">Name</Label>
+          <Input type="text" name="name" onChange={handleChange} value={newDog.name} />
+        </FormGroup>
+      </Form>
+      <Form>
+        <FormGroup>
+          <Label for="age">Age</Label>
+          <Input type="text" name="age" onChange={handleChange} value={newDog.age}  />
+        </FormGroup>
+      </Form>
+      <Form>
+        <FormGroup>
+          <Label for="enjoys">Enjoys</Label>
+          <Input type="text" name="enjoys" onChange={handleChange} value={newDog.enjoys}  />
+        </FormGroup>
+      </Form>
+      <Form>
+        <FormGroup>
+          <Label for="image">Image</Label>
+          <Input type="text" name="image" onChange={handleChange} value={newDog.image}  />
+        </FormGroup>
+      </Form>
+      <div>
+        <Button onClick={handleSubmit} name="submit">
+          Submit Dog
+        </Button>
+      </div>
+    </>
+    )
 }
 
 export default DogNew;
