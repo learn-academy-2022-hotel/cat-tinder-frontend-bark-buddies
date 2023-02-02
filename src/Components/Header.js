ffcb10fc05bcb17257921d921c9e988535a51a2a
oagomezz <ogomezbarragan437@gmail.com>
2023-02-02 09:35:10 -0800

    Completed header and footer testing

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,10 +1,44 @@
 import React from 'react'
+import { Nav, NavItem } from "reactstrap"
+import { NavLink } from "react-router-dom"
+import dogLogo from "../assets/dogLogo.jpg"
+
 
 const Header = () => {
     return(
-        <>
-            <h1 className = "header" >HEADER!</h1>
-        </>
+      
+        <Nav className="header-nav">
+        <NavItem>
+          <NavLink to="/">
+            <img
+              src={dogLogo}
+              alt="Dog Tinder logo with outline of Dog"
+              className="Dog-logo"
+            />
+          </NavLink>
+        </NavItem>
+        <NavItem>
+          <NavLink to="/dogindex" className="nav-link">
+            Meet the Dogs
+          </NavLink>
+        </NavItem>
+        <NavItem>
+          <NavLink to="/dognew" className="nav-link">
+            Add a New Dog
+          </NavLink>
+        </NavItem>
+        <NavItem>
+            <a
+                target="blank"
+                href="https://www.aspca.org/adopt-pet/adoptable-dogs-your-local-shelter"
+                className="nav-link"
+            >
+                Adopt a Dog!
+            </a>
+
+        </NavItem>
+        </Nav>
+
     )
 }
 
